e49c749b88205776b003368f4ac0a559a2c203cb
harry <harrisongitau3@gmail.com>
2023-03-22 22:17:27 +0300

    Define a student class

---
+++ registerStudent/src/Student.java
@@ -0,0 +1,69 @@
+// Define a student to register
+public class Student {
+    // Class attributes used to define a student
+    private String first_name, last_name, program;
+    private int id;
+    private double score;
+
+    // Setters and getters to access private variables
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+    public double getScore() {
+        return score;
+    }
+
+    // Constructor that defines a student object
+    Student(String _first_name, String _last_name, int _id, String _program, double _score){
+        this.first_name=_first_name;
+        this.last_name=_last_name;
+        this.id=_id;
+        this.program=_program;
+        this.score=_score;
+
+
+    }
+    
+    // Show student data
+    public  void display(){
+        System.out.println(
+            "First Name: " + getFirstName() + 
+            " Last Name: " + getLastName() + 
+            " ID: " + getId() + 
+            " Score: "+ getScore() + 
+            " Program: "+ getProgram());;
+
+
+    }
+}
